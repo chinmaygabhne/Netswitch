5deeb48e710bffd782c56be451828453e2597005
chinmaygabhne <wongstrange18april@gmailcom>
2026-04-10 11:53:58 +0530

    Changes to be committed: 	modified:   netswitchroom.bat

--- netswitchroom.bat
+++ netswitchroom.bat
@@ -5,7 +5,7 @@ set eth=Ethernet
 
 echo Switching to HOME network (DHCP)...
 
-netsh interface ip set address name="%eth%" static 10.0.112.133 255.255.248.0 10.0.8112.1
+netsh interface ip set address name="%eth%" static 10.0.112.133 255.255.248.0 10.0.112.1
 netsh interface ip set dns name="%eth%" static 1.1.1.1
 
 echo.
