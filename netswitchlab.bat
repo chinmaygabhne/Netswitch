de3a527c242e4a551a459124b2db8e814ea15310
chinmaygabhne <wongstrange18april@gmailcom>
2026-04-20 16:01:37 +0530

    ip changed according to availablity

--- netswitchlab.bat
+++ netswitchlab.bat
@@ -5,7 +5,7 @@ set eth=Ethernet
 
 echo Switching to LAB network...
 
-netsh interface ip set address name="%eth%" static 10.2.11.80 255.255.252.0 10.2.8.1
+netsh interface ip set address name="%eth%" static 10.2.11.81 255.255.252.0 10.2.8.1
 netsh interface ip set dns name="%eth%" static 8.8.8.8
 
 echo.
